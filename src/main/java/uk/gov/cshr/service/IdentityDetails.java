caa9c16369ca2a0e4af8d3ed3b3992bcfe001be5
Max Stewart <max@cautionyourblast.com>
2018-04-10 21:38:04 +0100

    First pass at implementing an oauth server.
    Client and Identity authentication working, token store implementation pending.

---
+++ src/main/java/uk/gov/cshr/service/IdentityDetails.java
@@ -0,0 +1,57 @@
+package uk.gov.cshr.service;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import uk.gov.cshr.domain.Identity;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toSet;
+
+public class IdentityDetails implements UserDetails {
+
+    private Identity identity;
+
+    public IdentityDetails(Identity identity) {
+        this.identity = identity;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return identity.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(toSet());
+    }
+
+    @Override
+    public String getPassword() {
+        return identity.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return identity.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
