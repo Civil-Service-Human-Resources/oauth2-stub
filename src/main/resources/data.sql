f88cab577cd5dea7f7c0bdadd5ce695fcaa8051c
Max Stewart <max@cautionyourblast.com>
2018-04-12 15:49:46 +0100

    Add ROLE_ again just for now :)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,8 +9,8 @@ INSERT INTO `client` (active, uid, password)
 VALUES (true, '9fbd4ae2-2db3-44c7-9544-88e80255b56e', '$2a$10$AbxhLGtIx7yv8jhF0BePiOxnb1mlHHq/Ge4R3PxCL2wIsoEov1VaS');
 
 INSERT INTO `role` (name) VALUES
-('USER'),
-('SUPER_USER');
+('ROLE_USER'),
+('ROLE_SUPER_USER');
 
 INSERT INTO `identity` (active, email, uid, password) VALUES
 (true, 'user@domain.com', '3c706a70-3fff-4e7b-ae7f-102c1d46f569', '$2a$10$sGfnyPnJ8a0b9R.vqIphKu5vjetS3.Bvi6ISv39bOphq5On0U2m36'),
