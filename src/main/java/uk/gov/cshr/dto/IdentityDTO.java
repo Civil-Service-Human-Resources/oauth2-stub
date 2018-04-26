e331ec71a3b76a1ec9bf2ce08cc33423bfa71bec
Yuksel Kurtbas <yukselk@kainos.com>
2018-04-26 11:58:40 +0100

    [LPFG-104][LPFG-178][OPTIMIZE] Unused imports

--- src/main/java/uk/gov/cshr/dto/IdentityDTO.java
+++ src/main/java/uk/gov/cshr/dto/IdentityDTO.java
@@ -3,9 +3,7 @@ package uk.gov.cshr.dto;
 import uk.gov.cshr.domain.Identity;
 import uk.gov.cshr.domain.Role;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class IdentityDTO {
