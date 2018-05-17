3fa41a7adb843876b4caf75e21c182a79dd44099
Yuksel Kurtbas <yukselk@kainos.com>
2018-05-17 11:07:48 +0100

    [LPFG-347][CHORE] Update roles

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,17 +10,17 @@ VALUES (true, '9fbd4ae2-2db3-44c7-9544-88e80255b56e', '$2a$10$AbxhLGtIx7yv8jhF0B
 (true, 'f90a4080-e5e9-4a80-ace4-f738b4c9c30e', '$2a$10$AbxhLGtIx7yv8jhF0BePiOxnb1mlHHq/Ge4R3PxCL2wIsoEov1VaS', 'http://admin.local.cshr.digital/authenticate');
 
 INSERT INTO `role` (name) VALUES
-('USER'),
+('LEARNER'),
 ('COURSE_MANAGER'),
 ('IDENTITY_MANAGER');
 
 INSERT INTO `identity` (active, email, uid, password) VALUES
-(true, 'user@domain.com', '3c706a70-3fff-4e7b-ae7f-102c1d46f569', '$2a$10$sGfnyPnJ8a0b9R.vqIphKu5vjetS3.Bvi6ISv39bOphq5On0U2m36'),
+(true, 'leaner@domain.com', '3c706a70-3fff-4e7b-ae7f-102c1d46f569', '$2a$10$sGfnyPnJ8a0b9R.vqIphKu5vjetS3.Bvi6ISv39bOphq5On0U2m36'),
 (true, 'course-manager@domain.com', '8dc80f78-9a52-4c31-ac54-d280a70c18eb', '$2a$10$sGfnyPnJ8a0b9R.vqIphKu5vjetS3.Bvi6ISv39bOphq5On0U2m36'),
 (true, 'identity-manager@domain.com', '65313ea4-59ea-4802-a521-71f9a92c85cd', '$2a$10$sGfnyPnJ8a0b9R.vqIphKu5vjetS3.Bvi6ISv39bOphq5On0U2m36');
 
 INSERT INTO `identity_role` (identity_id, role_id) VALUES
-((SELECT id FROM identity WHERE email = 'user@domain.com'), (SELECT id FROM role WHERE name = 'USER')),
+((SELECT id FROM identity WHERE email = 'leaner@domain.com'), (SELECT id FROM role WHERE name = 'LEARNER')),
 ((SELECT id FROM identity WHERE email = 'course-manager@domain.com'), (SELECT id FROM role WHERE name = 'COURSE_MANAGER')),
 ((SELECT id FROM identity WHERE email = 'identity-manager@domain.com'), (SELECT id FROM role WHERE name = 'IDENTITY_MANAGER'));
 
