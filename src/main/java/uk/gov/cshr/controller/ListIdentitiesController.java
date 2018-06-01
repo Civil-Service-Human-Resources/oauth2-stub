feb3b7f0c1a2f3593451483b739ccdf727da7b17
Max Stewart <max@cautionyourblast.com>
2018-06-01 12:31:13 +0100

    Add endpoint to check line manager email address

--- src/main/java/uk/gov/cshr/controller/ListIdentitiesController.java
+++ src/main/java/uk/gov/cshr/controller/ListIdentitiesController.java
@@ -2,9 +2,10 @@ package uk.gov.cshr.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.cshr.domain.Identity;
 import uk.gov.cshr.dto.IdentityDTO;
 import uk.gov.cshr.repository.IdentityRepository;
 
@@ -29,5 +30,15 @@ public class ListIdentitiesController {
         return ResponseEntity.ok(StreamSupport.stream(identityRepository.findAll().spliterator(), false)
                 .map(IdentityDTO::new)
                 .collect(toList()));
+    }
+
+    @GetMapping(value = "/api/identities", params = "emailAddress")
+    public ResponseEntity<IdentityDTO> findByEmailAddress(@RequestParam String emailAddress) {
+
+        Identity identity = identityRepository.findFirstByActiveTrueAndEmailEquals(emailAddress);
+        if (identity != null) {
+            return ResponseEntity.ok(new IdentityDTO(identity));
+        }
+        return ResponseEntity.notFound().build();
     }
 }
