3bb495494e8a4b9c0321e322712b29bc62a37c92
Max Stewart <max@cautionyourblast.com>
2018-06-26 14:19:35 +0100

    Add booking feed user.

---
+++ src/main/resources/db/migration/mysql/V1.2.0__add-bookings-reporter.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO `role` (name) VALUES ('DOWNLOAD_BOOKING_FEED');
+
+INSERT INTO `identity` (active, email, uid, password) VALUES
+(true, 'booking-feed@domain.com', 'a4cb1208-eca7-46a6-b496-0f6f354c6eac', '$2a$10$sGfnyPnJ8a0b9R.vqIphKu5vjetS3.Bvi6ISv39bOphq5On0U2m36')
+;
+
+INSERT INTO `identity_role` (identity_id, role_id) VALUES
+((SELECT id FROM identity WHERE email = 'booking-feed@domain.com'), (SELECT id FROM role WHERE name = 'DOWNLOAD_BOOKING_FEED'))
+;
