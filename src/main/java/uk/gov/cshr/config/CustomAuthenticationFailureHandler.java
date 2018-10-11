7eba3ca25545ceb2f44fa93f3f5bb648102a98cf
Rob Leney <roble@kainos.com>
2018-10-11 13:16:46 +0100

    LPFG-363: Added CustomAuthenticationFailureHandler to display custom message if account is locked.

---
+++ src/main/java/uk/gov/cshr/config/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,20 @@
+package uk.gov.cshr.config;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        if (exception.getMessage().equals("User account is locked")) {
+            response.sendRedirect("/login?error=locked");
+        } else {
+            response.sendRedirect("/login?error=failed");
+        }
+    }
+}
