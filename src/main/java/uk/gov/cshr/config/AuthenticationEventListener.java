663ee951da77a9a4866f8bde0c6f9bf3c8604529
Rob Leney <roble@kainos.com>
2018-10-29 12:26:09 +0000

    trial-cache-removal: Set dependencies in constructor and remove @Autowire annotation

--- src/main/java/uk/gov/cshr/config/AuthenticationEventListener.java
+++ src/main/java/uk/gov/cshr/config/AuthenticationEventListener.java
@@ -1,6 +1,5 @@
 package uk.gov.cshr.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.stereotype.Component;
@@ -10,8 +9,11 @@ import uk.gov.cshr.service.security.LoginAttemptService;
 @Component
 public class AuthenticationEventListener implements ApplicationListener<AuthenticationSuccessEvent> {
 
-    @Autowired
-    private LoginAttemptService loginAttemptService;
+    private final LoginAttemptService loginAttemptService;
+
+    public AuthenticationEventListener(LoginAttemptService loginAttemptService) {
+        this.loginAttemptService = loginAttemptService;
+    }
 
     public void onApplicationEvent(AuthenticationSuccessEvent e) {
         Object principal = e.getAuthentication().getPrincipal();
--- src/main/java/uk/gov/cshr/config/AuthenticationFailureListener.java
+++ src/main/java/uk/gov/cshr/config/AuthenticationFailureListener.java
@@ -1,6 +1,5 @@
 package uk.gov.cshr.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
 import org.springframework.stereotype.Component;
@@ -9,8 +8,11 @@ import uk.gov.cshr.service.security.LoginAttemptService;
 @Component
 public class AuthenticationFailureListener implements ApplicationListener<AuthenticationFailureBadCredentialsEvent> {
 
-    @Autowired
-    private LoginAttemptService loginAttemptService;
+    private final LoginAttemptService loginAttemptService;
+
+    public AuthenticationFailureListener(LoginAttemptService loginAttemptService) {
+        this.loginAttemptService = loginAttemptService;
+    }
 
     public void onApplicationEvent(AuthenticationFailureBadCredentialsEvent e) {
         String email = (String) e.getAuthentication().getPrincipal();
