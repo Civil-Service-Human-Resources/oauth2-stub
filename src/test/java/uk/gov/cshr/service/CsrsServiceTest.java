9db095d9ab01506143c21af150034e93d6f6eabb
Daniel Keenan <danielkee@kainos.com>
2019-08-12 16:29:22 +0100

    Update unit test in line with new CsrsService constructor

--- src/test/java/uk/gov/cshr/service/CsrsServiceTest.java
+++ src/test/java/uk/gov/cshr/service/CsrsServiceTest.java
@@ -1,44 +1,44 @@
-//package uk.gov.cshr.service;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.Mock;
-//import org.mockito.junit.MockitoJUnitRunner;
-//import org.springframework.web.client.RestTemplate;
-//import uk.gov.cshr.domain.AgencyToken;
-//
-//import java.util.Optional;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.mockito.Mockito.when;
-//
-//@RunWith(MockitoJUnitRunner.class)
-//public class CsrsServiceTest {
-//
-//    @Mock
-//    private RestTemplate restTemplate;
-//
-//    private String agencyTokensFormat;
-//
-//    private CsrsService csrsService;
-//
-//    @Before
-//    public void setUp() {
-//        agencyTokensFormat = "http://locahost:9002/agencyTokens?domain=%s&token=%s&code=%s";
-//        csrsService = new CsrsService(restTemplate, agencyTokensFormat);
-//    }
-//
-//    @Test
-//    public void shouldReturnAgencyTokenForDomainTokenOrganisation() {
-//        AgencyToken agencyToken = new AgencyToken();
-//        String domain = "example.com";
-//        String token = "token123";
-//        String code = "code";
-//        Optional<AgencyToken> optionalAgencyToken = Optional.of(agencyToken);
-//
-//        when(restTemplate.getForObject(String.format(agencyTokensFormat, domain, token, code), AgencyToken.class)).thenReturn(agencyToken);
-//
-//        assertEquals(optionalAgencyToken, csrsService.getAgencyTokenForDomainTokenOrganisation(domain, token, code));
-//    }
-//}
+package uk.gov.cshr.service;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.web.client.RestTemplate;
+import uk.gov.cshr.domain.AgencyToken;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CsrsServiceTest {
+
+    @Mock
+    private RestTemplate restTemplate;
+    private String agencyTokensFormat;
+    private String agencyTokensByDomainFormat;
+    private String organisationalUnitsFlatUrl;
+    private CsrsService csrsService;
+
+    @Before
+    public void setUp() {
+        agencyTokensFormat = "http://locahost:9002/agencyTokens?domain=%s&token=%s&code=%s";
+        csrsService = new CsrsService(restTemplate, agencyTokensFormat, agencyTokensByDomainFormat, organisationalUnitsFlatUrl);
+    }
+
+    @Test
+    public void shouldReturnAgencyTokenForDomainTokenOrganisation() {
+        AgencyToken agencyToken = new AgencyToken();
+        String domain = "example.com";
+        String token = "token123";
+        String code = "code";
+        Optional<AgencyToken> optionalAgencyToken = Optional.of(agencyToken);
+
+        when(restTemplate.getForObject(String.format(agencyTokensFormat, domain, token, code), AgencyToken.class)).thenReturn(agencyToken);
+
+        assertEquals(optionalAgencyToken, csrsService.getAgencyTokenForDomainTokenOrganisation(domain, token, code));
+    }
+}
