78d5c57d473638efa7ae98e83599199886676bdd
matthewmu <matthewmu@kainos.com>
2019-11-06 13:49:39 +0000

    fix-maintenance-page-v3

--- src/main/java/uk/gov/cshr/controller/LoginController.java
+++ src/main/java/uk/gov/cshr/controller/LoginController.java
@@ -1,7 +1,6 @@
 package uk.gov.cshr.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.web.savedrequest.DefaultSavedRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,12 +16,12 @@ public class LoginController {
 
     @RequestMapping("/login")
     public String login( HttpServletRequest request, HttpServletResponse response) throws IOException {
-        DefaultSavedRequest dsr = (DefaultSavedRequest) request.getSession().getAttribute("SPRING_SECURITY_SAVED_REQUEST");
-        if (dsr != null && dsr.getQueryString() == null) {
-            response.sendRedirect(lpgUiUrl);
-        }
+//        DefaultSavedRequest dsr = (DefaultSavedRequest) request.getSession().getAttribute("SPRING_SECURITY_SAVED_REQUEST");
+//        if (dsr != null && dsr.getQueryString() == null) {
+//            response.sendRedirect(lpgUiUrl);
+//        }
 
-        return "login";
+        return "maintenance";
 
     }
 
