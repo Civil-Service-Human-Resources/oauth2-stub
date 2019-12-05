434119a2aaba2e55f46e1076473dd5c55beedcc7
bridgeth <b.hodgson@kainos.com>
2019-12-05 16:13:50 +0000

    LC-337 - Added unit tests for redirect controller

--- src/main/java/uk/gov/cshr/controller/emailUpdate/RedirectController.java
+++ src/main/java/uk/gov/cshr/controller/emailUpdate/RedirectController.java
@@ -1,20 +1,16 @@
 package uk.gov.cshr.controller.emailUpdate;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
-import java.net.URI;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.net.URISyntaxException;
 
 @Controller
@@ -44,13 +40,8 @@ public class RedirectController {
     }
 
     @RequestMapping("/redirectToUIChangeOrgPage")
-    public ResponseEntity<Object> redirectToExternalUrl() throws URISyntaxException {
-        URI ui = new URI(lpgUiUrl);
-        UriComponents uriComponents = UriComponentsBuilder.newInstance().uri(ui)
-                .path("/").query("updateOrg={keyword}").buildAndExpand("true");
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setLocation(uriComponents.toUri());
-        return new ResponseEntity<>(httpHeaders, HttpStatus.SEE_OTHER);
+    public void goToChangeOrgPageInLPGUI(HttpServletResponse response) throws URISyntaxException, IOException {
+        response.sendRedirect(lpgUiUrl + "?updateOrg=true");
     }
 
 }
--- src/test/java/uk/gov/cshr/controller/emailUpdate/RedirectControllerTest.java
+++ src/test/java/uk/gov/cshr/controller/emailUpdate/RedirectControllerTest.java
@@ -1,4 +1,57 @@
 package uk.gov.cshr.controller.emailUpdate;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.transaction.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
 public class RedirectControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Value("${lpg.uiUrl}")
+    private String lpgUiUrl;
+
+    @Test
+    public void whenInvalidEmail_shouldRedirectToTheLogoutPage() throws Exception {
+        mockMvc.perform(
+                get("/invalid"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/logout"));
+
+    }
+
+    @Test
+    public void whenGoToUIHomePage_shouldRedirectToTheLPGUIHomePage() throws Exception {
+        mockMvc.perform(
+                get("/redirectToUIHomePage"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl(lpgUiUrl))
+                .andExpect(model().size(0));
+    }
+
+    @Test
+    public void whenGoToChangeOrgPageInLPGUI_shouldRedirectToTheLPGUIChangeOrgPage() throws Exception {
+        String expectedRedirectToUrl = lpgUiUrl+"?updateOrg=true";
+
+        mockMvc.perform(
+                get("/redirectToUIChangeOrgPage"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl(expectedRedirectToUrl));
+    }
+
 }
