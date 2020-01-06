2035e8ca6f386935670b962009c6efdc47bdef38
bridgeth <b.hodgson@kainos.com>
2020-01-06 16:05:12 +0000

    LC-14 - Fixed AccountController Unit Test.

--- src/test/java/uk.gov.cshr/controller/AccountControllerThreeTest.java
+++ src/test/java/uk.gov.cshr/controller/AccountControllerThreeTest.java
@@ -7,27 +7,29 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import uk.gov.cshr.config.SpringSecurityTestConfig;
+import uk.gov.cshr.domain.Identity;
+import uk.gov.cshr.service.EmailUpdateService;
 import uk.gov.cshr.service.security.IdentityDetails;
+import uk.gov.cshr.service.security.IdentityService;
 import uk.gov.cshr.utils.WithMockCustomUser;
 
 import javax.servlet.Filter;
 
-import java.security.Principal;
-
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -51,6 +53,12 @@ public class AccountControllerThreeTest {
     @Autowired
     private UserDetailsService userDetailsService;
 
+    @MockBean
+    private EmailUpdateService emailUpdateService;
+
+    @MockBean
+    private IdentityService identityService;
+
     @Before
     public void setup() {
         mockMvc = MockMvcBuilders
@@ -69,6 +77,8 @@ public class AccountControllerThreeTest {
     public void shouldUpdateEmailIfCodeIsValid1() throws Exception {
 
         prepareSecurityContext("uid");
+        when(emailUpdateService.verifyCode(any(Identity.class), anyString())).thenReturn(true);
+        String expectedCode = "1234567891234567";
 
         mockMvc.perform(get("/account/email/verify/1234567891234567")
                 .with(request1 -> {
@@ -76,15 +86,11 @@ public class AccountControllerThreeTest {
                             return request1;
                         }
                 ))
-                .andDo(print());
-
-
-     /*   mockMvc.perform(get("/account/email/verify/1234567891234567"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/account/emailUpdated"))
                 .andDo(print());
-*/
-        //verify(emailUpdateService, times(1)).updateEmailAddress(eq(identity), eq(expectedCode));
+
+        verify(emailUpdateService, times(1)).updateEmailAddress(any(Identity.class), eq(expectedCode));
     }
 
     private void prepareSecurityContext(String userNameToAuthenticateWith) {
