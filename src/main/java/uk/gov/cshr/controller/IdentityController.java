a7ef167be5f0ca4371e3a095ec4e0d299f3833b3
bridgeth <b.hodgson@kainos.com>
2020-01-23 11:06:31 +0000

    LC-337 - removed unused code

--- src/main/java/uk/gov/cshr/controller/IdentityController.java
+++ src/main/java/uk/gov/cshr/controller/IdentityController.java
@@ -3,8 +3,6 @@ package uk.gov.cshr.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
