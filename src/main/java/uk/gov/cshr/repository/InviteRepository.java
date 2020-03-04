fce31c936759b62e334ee1437decfcee421e910c
matthewmu <matthewmu@kainos.com>
2020-03-04 17:02:58 +0000

    LC-14: removing profile annotation from InviteService. @profile annotation currently causing issues in environments. Removing for now until we can figure a way of handling in both mocks and environments

--- src/main/java/uk/gov/cshr/repository/InviteRepository.java
+++ src/main/java/uk/gov/cshr/repository/InviteRepository.java
@@ -1,12 +1,10 @@
 package uk.gov.cshr.repository;
 
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import uk.gov.cshr.domain.Invite;
 import uk.gov.cshr.domain.InviteStatus;
 
-@Profile({"default"})
 @Repository
 public interface InviteRepository extends CrudRepository<Invite, Long> {
 
