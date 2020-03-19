a2431aaa326269ddc3e5b48e6d068e30c501d5f9
Matt Doherty <matthew.doherty@kainos.com>
2020-03-19 11:36:16 +0000

    [lc-335] - Change TokenRequest to implement the interface serializable, issue seen with Spring Redis deserialization not being able to deserialize the object

--- src/main/java/uk/gov/cshr/domain/TokenRequest.java
+++ src/main/java/uk/gov/cshr/domain/TokenRequest.java
@@ -1,11 +1,14 @@
 package uk.gov.cshr.domain;
 
+import java.io.Serializable;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-public class TokenRequest {
+public class TokenRequest implements Serializable {
+
+    private static long serialVersionUID = 1l;
 
     private String domain;
     private String token;
