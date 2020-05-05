Civil-Service-Human-Resources/identity-service

Disable whitelabel error banner in springboot app
Error-management-identity-service

src/main/java/uk/gov/cshr/controller/LoginController.java
@@ -4,30 +4,27 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.web.savedrequest.DefaultSavedRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Controller
 public class LoginController {
 
-    @Value("${lpg.uiUrl}")
-    private String lpgUiUrl;
+  @Value("${lpg.uiUrl}")
+  private String lpgUiUrl;
 
-    @RequestMapping("/login")
-    public String login( HttpServletRequest request, HttpServletResponse response) throws IOException {
-        DefaultSavedRequest dsr = (DefaultSavedRequest) request.getSession().getAttribute("SPRING_SECURITY_SAVED_REQUEST");
-        if (dsr != null && dsr.getQueryString() == null) {
-            response.sendRedirect(lpgUiUrl);
-        }
-
-        return "login";
-
+  @RequestMapping("/login")
+  public String login(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    DefaultSavedRequest dsr =
+        (DefaultSavedRequest) request.getSession().getAttribute("SPRING_SECURITY_SAVED_REQUEST");
+    if (dsr != null && dsr.getQueryString() == null) {
+      response.sendRedirect(lpgUiUrl);
     }
+    return "login";
+  }
 
-    @RequestMapping("/management/login")
-    public String managementLogin() {
-        return "management-login";
-    }
+  @RequestMapping("/management/login")
+  public String managementLogin() {
+    return "management-login";
+  }
 }

Civil-Service-Human-Resources/oauth2-stub

src/main/java/uk/gov/cshr/controller/LoginController.java
@@ -1,33 +1,46 @@
 package uk.gov.cshr.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.web.savedrequest.DefaultSavedRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
 @Controller
 public class LoginController {
 
-    @Value("${lpg.uiUrl}")
-    private String lpgUiUrl;
+  @Value("${lpg.uiUrl}")
+  private String lpgUiUrl;
 
-    @RequestMapping("/login")
-    public String login( HttpServletRequest request, HttpServletResponse response) throws IOException {
-        DefaultSavedRequest dsr = (DefaultSavedRequest) request.getSession().getAttribute("SPRING_SECURITY_SAVED_REQUEST");
-        if (dsr != null && dsr.getQueryString() == null) {
-            response.sendRedirect(lpgUiUrl);
-        }
-
-        return "login";
-
+  /**
+   *
+   * @param request
+   * @param response
+   * @return
+   * @throws IOException
+   * Login interface.
+   */
+  @RequestMapping("/login")
+  public String login(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    DefaultSavedRequest dsr =
+        (DefaultSavedRequest) request.getSession().getAttribute("SPRING_SECURITY_SAVED_REQUEST");
+    if (dsr != null && dsr.getQueryString() == null) {
+      response.sendRedirect(lpgUiUrl);
     }
+    return "login";
+  }
 
-    @RequestMapping("/management/login")
-    public String managementLogin() {
-        return "management-login";
-    }
+  /**
+   *
+   * @return
+   * A method to redirect management api logins
+   */
+  @RequestMapping("/management/login")
+  public String managementLogin() {
+    return "management-login";
+  }
 }