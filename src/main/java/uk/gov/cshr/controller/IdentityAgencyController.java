55777395961d0cb9d52718ec56401a7c7d6e90b2
bridgeth <b.hodgson@kainos.com>
2020-06-05 15:10:40 +0100

    lc-14 - Fixing media type

--- src/main/java/uk/gov/cshr/controller/IdentityAgencyController.java
+++ src/main/java/uk/gov/cshr/controller/IdentityAgencyController.java
@@ -2,6 +2,7 @@ package uk.gov.cshr.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uk.gov.cshr.domain.Identity;
@@ -12,7 +13,6 @@ import java.util.Optional;
 
 @Slf4j
 @RestController
-@RequestMapping("/identity/agency")
 public class IdentityAgencyController {
 
     private IdentityRepository identityRepository;
@@ -22,13 +22,20 @@ public class IdentityAgencyController {
         this.identityRepository = identityRepository;
     }
 
-    @GetMapping("/{uid}")
+    @GetMapping(value = "/identity/agency/{uid}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<IdentityAgencyDTO> findByUid(@PathVariable String uid) {
         log.info("Getting agency token uid for user with uid " + uid);
         Optional<Identity> identity = identityRepository.findFirstByUid(uid);
         return identity
-                .map(i -> ResponseEntity.ok(new IdentityAgencyDTO(i.getUid(), i.getAgencyTokenUid())))
+                .map(i -> buildResponse(i))
                 .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    private ResponseEntity<IdentityAgencyDTO> buildResponse(Identity i) {
+        IdentityAgencyDTO responseDTO = new IdentityAgencyDTO();
+        responseDTO.setAgencyTokenUid(i.getAgencyTokenUid());
+        responseDTO.setUid(i.getUid());
+        return ResponseEntity.ok(responseDTO);
     }
 
 }
--- src/main/java/uk/gov/cshr/dto/IdentityAgencyDTO.java
+++ src/main/java/uk/gov/cshr/dto/IdentityAgencyDTO.java
@@ -1,18 +1,11 @@
 package uk.gov.cshr.dto;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
 @Data
 public class IdentityAgencyDTO {
 
     private String uid;
     private String agencyTokenUid;
-
-    public IdentityAgencyDTO(String uid, String agencyTokenUid) {
-        this.uid = uid;
-        this.agencyTokenUid = agencyTokenUid;
-    }
 
 }
