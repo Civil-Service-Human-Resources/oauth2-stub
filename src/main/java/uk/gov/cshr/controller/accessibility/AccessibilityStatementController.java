4cb19642cc534cecbb805475e5b4bf0c8c9548dd
Jain Neeraj <neeraj.jain@cabinetoffice.gov.uk>
2020-09-18 09:40:36 +0100

    LC-688: accessibility statement published

--- src/main/java/uk/gov/cshr/controller/accessibility/AccessibilityStatementController.java
+++ src/main/java/uk/gov/cshr/controller/accessibility/AccessibilityStatementController.java
@@ -1,12 +1,10 @@
 package uk.gov.cshr.controller.accessibility;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Slf4j
 @Controller
 @RequestMapping("/accessibility")
 public class AccessibilityStatementController {
@@ -17,7 +15,7 @@ public class AccessibilityStatementController {
     }
 
     @GetMapping(path = "/statement")
-    public String requestInvite(Model model) {
+    public String showAccessibilityStatement(Model model) {
         return ACCESSIBILITY_STATEMENT_TEMPLATE;
     }
 }
