96f87f2efd3daf64e6f2c87ea4ec0d6321e58596
Jain Neeraj <neeraj.jain@cabinetoffice.gov.uk>
2021-01-28 16:41:57 +0000

    idt-release-2.0-maintenance-page

--- src/main/java/uk/gov/cshr/controller/LoginController.java
+++ src/main/java/uk/gov/cshr/controller/LoginController.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.web.savedrequest.DefaultSavedRequest;
+//import org.springframework.security.web.savedrequest.DefaultSavedRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,12 +18,13 @@ public class LoginController {
 
   @RequestMapping("/login")
   public String login(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    DefaultSavedRequest dsr =
-        (DefaultSavedRequest) request.getSession().getAttribute("SPRING_SECURITY_SAVED_REQUEST");
-    if (dsr != null && dsr.getQueryString() == null) {
-      response.sendRedirect(lpgUiUrl);
-    }
-    return "login";
+    // DefaultSavedRequest dsr =
+    //     (DefaultSavedRequest) request.getSession().getAttribute("SPRING_SECURITY_SAVED_REQUEST");
+    // if (dsr != null && dsr.getQueryString() == null) {
+    //   response.sendRedirect(lpgUiUrl);
+    // }
+    // return "login";
+    return "maintenance";
   }
 
   @RequestMapping("/management/login")
